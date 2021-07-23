eeb2e2584a66378f1df52d0460f67f60a25fae95
ETCasual <casualorient@gmail.com>
2021-07-24 07:01:10 +0800

    Added all day 2 and mutation events announcements

--- src/commands/AnnounceCommands/AnnounceCommand.ts
+++ src/commands/AnnounceCommands/AnnounceCommand.ts
@@ -62,6 +62,58 @@ export const run: RunFunction = async (client, message, args) => {
 				);
 			}
 			break;
+
+		case 'observe':
+			if (isOwner()) {
+				await announcementChannel.send(
+					'@everyone',
+					client.emptyEmbed({
+						title: 'Observation is key!',
+						description:
+							'Study shows the virus has a weakness!\nObserve the virus and study its weakness!\nIt may be the final puzzle piece to unlock the vaccine formula!\n\n许多的研究显示这个病毒是存有弱点的!\n我们必须仔细观察病毒并且研究它的弱点!\n这也许能为解锁疫苗的蓝图做最大的贡献!\n\n*Hint: Click the Title Text to navigate to website!*\n*温馨提示: 点击此标题以进入网站!*',
+						url: 'https://thecure-web.vercel.app/',
+					})
+				);
+			}
+			break;
+		case 'templerun':
+			if (isOwner()) {
+				await announcementChannel.send(
+					'@everyone',
+					client.emptyEmbed({
+						title: '[MUTATION EVENT] Lambda!',
+						description:
+							"Theres a new variant of the virus, called Lambda!\nIt is known for its immune evasion\nEvading immune systems renders the body into a weaker state!\nWe must **RACE** against time to defeat the variant!\n\n病毒变异了! 被赋予了 Lambda之名!\n此病毒特征是回避身体的免疫系统\n导致身体进入了虚弱的状态!\n研究员们必须立刻与时间 **赛跑** 以打败变异的病毒!\n\n1. Researchers must download **Temple Run**\n2. Each research team must accumulate more than **20,000 meters** in distance\n3. Screenshot your results with the distance visible and submit to your group's submission channel\n4. Only a maximum of 6 images allowed for submission!\n\n1. 研究员们必须下载 **Temple Run**\n2. 并且每研究团队需在游戏里获得不少于 **20,000米** 的距离\n3. 截图游戏的结果并确保距离数额需清楚可见\n4. 最多只允许 6 张图片作呈交",
+					})
+				);
+			}
+			break;
+		case 'crowdcity':
+			if (isOwner()) {
+				await announcementChannel.send(
+					'@everyone',
+					client.emptyEmbed({
+						title: '[MUTATION EVENT] Delta!',
+						description:
+							"With it's increased transmission rate,\nThe Delta variant is dangerously infectous to people\nWe must study its transmission rate and discover how to stop it!\n\n透过高度的散播率,\n变异病毒 Delta 已成为了现在传染性最强的病毒\n我们需要研究此变异病毒的感染率以了解如何击破它!\n\n1. Researchers must find a form of **Crowd City** game\n2. Each research team must accumulate more than **500** in score\n3. Screenshot your results and submit them in your group's submission channel\n4. Only a maximum of 6 images allowed for submission!\n\n1. 研究员们必须玩一款 **Crowd City** 游戏\n2. 并且每研究团队需在游戏里获得不少于 **500** 分\n3. 截图游戏的结果并确保距离数额需清楚可见\n4. 最多只允许 6 张图片作呈交",
+					})
+				);
+			}
+			break;
+
+		case 'findme':
+			if (isOwner()) {
+				await announcementChannel.send(
+					'@everyone',
+					client.emptyEmbed({
+						title: 'Virus Pattern!',
+						description:
+							'Seems like our researchers have found patterns on living bodies indicating the virus!\nQuickly proceed to the website and check on your tasks!\n\n看来有研究员发现了存活在生物上的病毒规律!\n赶快到网站检查你们的新任务吧!\n\n*Hint: Click the Title Text to navigate to website!*\n*温馨提示: 点击此标题以进入网站!*',
+						url: 'https://thecure-web.vercel.app/',
+					})
+				);
+			}
+			break;
 		case 'zoom':
 			if (isOwner()) {
 				await announcementChannel.send(
