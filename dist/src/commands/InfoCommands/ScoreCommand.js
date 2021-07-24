cde4838b15fb2ffa443065f93a2bfb36b8e1feb3
ETCasual <casualorient@gmail.com>
2021-07-24 21:49:24 +0800

    score command

--- dist/src/commands/InfoCommands/ScoreCommand.js
+++ dist/src/commands/InfoCommands/ScoreCommand.js
@@ -1,10 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.name = exports.run = void 0;
-const run = async (client, message, args) => {
+const run = async (client, message) => {
     await message.channel.send(
     // '@everyone',
-    client.emptyEmbed({}));
+    client.emptyEmbed({
+        title: 'Top 3',
+        description: 'As of findme.\n只到 findme 环节',
+        image: {
+            url: 'https://i.ibb.co/wY5JLTz/score.png',
+        },
+    }));
 };
 exports.run = run;
 exports.name = 'score';
--- src/commands/InfoCommands/ScoreCommand.ts
+++ src/commands/InfoCommands/ScoreCommand.ts
@@ -1,9 +1,15 @@
 import { RunFunction } from '../../interfaces/Command';
 
-export const run: RunFunction = async (client, message, args) => {
+export const run: RunFunction = async (client, message) => {
 	await message.channel.send(
 		// '@everyone',
-		client.emptyEmbed({})
+		client.emptyEmbed({
+			title: 'Top 3',
+			description: 'As of findme.\n只到 findme 环节',
+			image: {
+				url: 'https://i.ibb.co/wY5JLTz/score.png',
+			},
+		})
 	);
 };
 
